67b7a61ae83d98766579a5e87e3f642330b03ed5
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-11-16 20:52:27 +0900

    styled-components, styled-reset 설정 & theme.ts 파일 생성

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,10 +1,16 @@
 import React, { PropsWithChildren } from "react";
-import AppPresenter from "./AppPresenter";
 import { DataProps, graphql, MutateProps } from "react-apollo";
+import { Reset } from 'styled-reset'
+import theme from "../../theme";
+import { ThemeProvider } from "../../typed-components";
+import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
 const AppContainer = ({ data }: PropsWithChildren<Partial<DataProps<{}, {}>> & Partial<MutateProps<{}, {}>>> | any) => (
-  <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+  <ThemeProvider theme={theme}>
+    <Reset />
+    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+  </ThemeProvider>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
---
+++ src/typed-components.ts
@@ -0,0 +1,16 @@
+import * as styledComponents from "styled-components";
+import { ThemedStyledComponentsModule } from "styled-components";
+
+interface IThemeInterface {
+  blueColor: string;
+}
+
+const {
+  default: styled,
+  css,
+  keyframes,
+  ThemeProvider
+} = styledComponents as ThemedStyledComponentsModule<IThemeInterface>;
+
+export { css, keyframes, ThemeProvider };
+export default styled;
