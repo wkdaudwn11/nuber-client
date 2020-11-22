afdb44f08d0d3db77a197410fd00497b2448099f
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-11-23 05:33:45 +0900

    핸드폰번호 로그인 - react-toast 적용

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,14 +1,21 @@
-import React, { PropsWithChildren } from "react";
-import { DataProps, graphql, MutateProps } from "react-apollo";
-import theme from "../../theme";
-import { ThemeProvider } from "../../typed-components";
-import AppPresenter from "./AppPresenter";
-import { IS_LOGGED_IN } from "./AppQueries";
+import React, { PropsWithChildren } from 'react';
+import { DataProps, graphql, MutateProps } from 'react-apollo';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import theme from '../../theme';
+import { ThemeProvider } from '../../typed-components';
+import AppPresenter from './AppPresenter';
+import { IS_LOGGED_IN } from './AppQueries';
 
-const AppContainer = ({ data }: PropsWithChildren<Partial<DataProps<{}, {}>> & Partial<MutateProps<{}, {}>>> | any) => (
-  <ThemeProvider theme={theme}>
-    <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
-  </ThemeProvider>
+const AppContainer = ({
+	data,
+}: PropsWithChildren<Partial<DataProps<{}, {}>> & Partial<MutateProps<{}, {}>>> | any) => (
+	<>
+		<ThemeProvider theme={theme}>
+			<AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+		</ThemeProvider>
+		<ToastContainer draggable={true} position="bottom-center" />
+	</>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import PhoneLoginPresenter from './PhoneLoginPresenter';
 
 type stateType = {
@@ -23,8 +24,14 @@ const PhoneLoginContainer = () => {
 	const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
 		e.preventDefault();
 		const { countryCode, phoneNumber } = state;
-		console.log('countryCode ', countryCode);
-		console.log('phoneNumber ', phoneNumber);
+		const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
+
+		if (isValid) {
+			toast.success('ok');
+			return;
+		} else {
+			toast.error('잘못된 핸드폰 번호 입니다.');
+		}
 	};
 
 	const { countryCode, phoneNumber } = state;
