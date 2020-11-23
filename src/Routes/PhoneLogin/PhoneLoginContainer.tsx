f31fc5e39bd4f48bec42e3af7b9711ba504eac2f
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-11-23 09:42:55 +0900

    핸드폰 로그인 API 연동 작업중 && Graphql 관련 Query, Muation들에 대한 Type을 자동으로 생성하기 위해 codegen 세팅

--- src/Routes/PhoneLogin/PhoneLoginContainer.tsx
+++ src/Routes/PhoneLogin/PhoneLoginContainer.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
+import { Mutation } from 'react-apollo';
 import { toast } from 'react-toastify';
 import PhoneLoginPresenter from './PhoneLoginPresenter';
+import { PHONE_SIGN_IN } from './PhoneQueries.queries';
 
 type stateType = {
 	countryCode: string;
 	phoneNumber: string;
 };
+
+interface IMutationInterface {
+	phoneNumber: string;
+}
+
+// class PhoneSignInMutation extends Mutation<any, IMutationInterface> {}
 
 const PhoneLoginContainer = () => {
 	const [state, setState] = useState<stateType>({
@@ -21,28 +29,50 @@ const PhoneLoginContainer = () => {
 		});
 	};
 
-	const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-		e.preventDefault();
-		const { countryCode, phoneNumber } = state;
-		const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
+	// const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+	// 	e.preventDefault();
+	// 	const { countryCode, phoneNumber } = state;
+	// 	const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
 
-		if (isValid) {
-			toast.success('ok');
-			return;
-		} else {
-			toast.error('잘못된 핸드폰 번호 입니다.');
-		}
-	};
+	// 	if (isValid) {
+	// 		toast.success('ok');
+	// 		return;
+	// 	} else {
+	// 		toast.error('잘못된 핸드폰 번호 입니다.');
+	// 	}
+	// };
 
 	const { countryCode, phoneNumber } = state;
 
 	return (
-		<PhoneLoginPresenter
-			countryCode={countryCode}
-			phoneNumber={phoneNumber}
-			onInputChange={onInputChange}
-			onSubmit={onSubmit}
-		/>
+		<Mutation<any, IMutationInterface>
+			mutation={PHONE_SIGN_IN}
+			variables={{
+				phoneNumber: `${countryCode}${phoneNumber}`,
+			}}
+		>
+			{(mutation, { loading }) => {
+				const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+					event.preventDefault();
+					const isValid = /^\+[1-9]{1}[0-9]{7,11}$/.test(`${countryCode}${phoneNumber}`);
+					if (isValid) {
+						alert('??');
+						mutation();
+					} else {
+						toast.error('Please write a valid phone number');
+					}
+				};
+				return (
+					<PhoneLoginPresenter
+						countryCode={countryCode}
+						phoneNumber={phoneNumber}
+						onInputChange={onInputChange}
+						onSubmit={onSubmit}
+						loading={loading}
+					/>
+				);
+			}}
+		</Mutation>
 	);
 };
 
